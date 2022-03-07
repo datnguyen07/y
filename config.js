a2e78cb0eb89e22d76bd3f4dd696b3b285479769
datnguyen07 <73738282+datnguyen07@users.noreply.github.com>
2022-03-07 22:38:57 +0700

    b

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Ly';
+const giftUrl = 'https://datnguyen07.github.io/l/';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Ly';
+const contentCard = 'Chúc Ly 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Ly trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
